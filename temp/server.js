ac238881f9f3141f3f7577b346d2cc081395c226
jtgjohn <gayj@rpi.edu>
2019-04-07 18:55:28 -0400

    gitignore

--- temp/server.js
+++ temp/server.js
@@ -3,110 +3,10 @@ var app = express();
 var path = require('path');
 var fetch = require('isomorphic-fetch');
 
-//const APIKey = "10028-2e5765d3-df29-400d-b607-722bbac2b14c";
+const APIKey = "10028-2e5765d3-df29-400d-b607-722bbac2b14c";
 const teamID = "681911804688300104";
 const featureID = "205%3A1595";
 
-
-//OAuth
-//Client Secret: xGVm6lOTv0do8ca7n0uQIisw6VLuwX
-//Client ID: Me3HgbzpUV5CYdvFfDwipX
-clientID = "Me3HgbzpUV5CYdvFfDwipX";
-clientSec = "xGVm6lOTv0do8ca7n0uQIisw6VLuwX";
-AccessToken = "";
-callback = "http://localhost:8080/contents.html"
-
-
-async function OAuthGetToken(code){
-    let result = await fetch('https://www.figma.com/api/oauth/token', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "client_id": clientID,
-            "client_secret": clientSec,
-            "redirect_uri": callback,
-            "code": code,
-            "grant_type": "authorization_code"
-        })
-    })
-
-    let ret = await result.json();
-    return ret;
-}
-
-//OAuth
-//=====================================================================================
-async function getUserAuth(){
-    //let result = await fetch('https://api.figma.com/v1/me/' + fileId , {
-    let result = await fetch('https://api.figma.com/v1/me/', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + AccessToken
-        }
-    })
-
-    let ret = await result.json()
-
-    return ret
-}
-
-async function getTeamProjectsAuth(teamId){
-    let result = await fetch('https://api.figma.com/v1/teams/' + teamId + "/projects", {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + AccessToken
-        }
-    })
-
-    let ret = await result.json()
-
-    return ret
-}
-
-async function getProjectFilesAuth(projectId){
-    let result = await fetch('https://api.figma.com/v1/projects/' + projectId + "/files", {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + AccessToken
-        }
-    })
-
-    let ret = await result.json()
-
-    return ret
-}
-
-async function getFileAuth(fileId){
-    let result = await fetch('https://api.figma.com/v1/files/' + fileId, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + AccessToken
-        }
-    })
-
-    let ret = await result.json()
-
-    return ret
-}
-
-async function getFileImagesAuth(fileId, ids){
-    let result = await fetch('https://api.figma.com/v1/images/' + fileId + "?ids=" + ids, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + AccessToken
-        }
-    })
-
-    let ret = await result.json()
-
-    return ret
-}
-
-
-//API Token
-//=====================================================================================
 async function getUser(){
     //let result = await fetch('https://api.figma.com/v1/me/' + fileId , {
     let result = await fetch('https://api.figma.com/v1/me/', {
@@ -178,18 +78,9 @@ app.get("/", async function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get("/contents.html", async function (req, res) {
-    //console.log(req);
-    //console.log(req["query"]["code"]);
-    result = await OAuthGetToken(req["query"]["code"]).catch(error => console.log(error));
-    //console.log(result);
-    AccessToken = result["access_token"];
-    res.sendFile(path.join(__dirname, "/contents.html"));
-});
-
 //user
 app.get("/user", async function (req, res) {
-    let result = await getUserAuth().catch(error => console.log(error));
+    let result = await getUser().catch(error => console.log(error));
     //console.log(JSON.stringify(result));
     let ret = "<html>";
     ret += "<body>";
@@ -204,24 +95,24 @@ app.get("/user", async function (req, res) {
 
 //team projects
 app.get("/teamProjects", async function (req, res) {
-    let result = await getTeamProjectsAuth(teamID).catch(error => console.log(error));
+    let result = await getTeamProjects(teamID).catch(error => console.log(error));
     
     res.send(JSON.stringify(result));
 });
 
 //project files
 app.get("/projectFiles", async function (req, res) {
-    let projects = await getTeamProjectsAuth(teamID).catch(error => console.log(error));
-    let result = await getProjectFilesAuth(projects["projects"][0]["id"]).catch(error => console.log(error));
+    let projects = await getTeamProjects(teamID).catch(error => console.log(error));
+    let result = await getProjectFiles(projects["projects"][0]["id"]).catch(error => console.log(error));
     
     res.send(JSON.stringify(result));
 });
 
 //File
 app.get("/file", async function (req, res) {
-    let projects = await getTeamProjectsAuth(teamID).catch(error => console.log(error));
-    let files = await getProjectFilesAuth(projects["projects"][0]["id"]).catch(error => console.log(error));
-    let result = await getFileAuth(files["files"][0]["key"]).catch(error => console.log(error));
+    let projects = await getTeamProjects(teamID).catch(error => console.log(error));
+    let files = await getProjectFiles(projects["projects"][0]["id"]).catch(error => console.log(error));
+    let result = await getFile(files["files"][0]["key"]).catch(error => console.log(error));
     
     let ret = "<html>";
     ret += "<body>";
@@ -238,9 +129,9 @@ app.get("/file", async function (req, res) {
 
 //FileImages
 app.get("/fileImage", async function (req, res) {
-    let projects = await getTeamProjectsAuth(teamID).catch(error => console.log(error));
-    let files = await getProjectFilesAuth(projects["projects"][0]["id"]).catch(error => console.log(error));
-    let result = await getFileImagesAuth(files["files"][0]["key"], featureID).catch(error => console.log(error));
+    let projects = await getTeamProjects(teamID).catch(error => console.log(error));
+    let files = await getProjectFiles(projects["projects"][0]["id"]).catch(error => console.log(error));
+    let result = await getFileImages(files["files"][0]["key"], featureID).catch(error => console.log(error));
     
     let ret = "<html>";
     ret += "<body>";
